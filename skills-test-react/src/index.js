0f0d9d27e569313c9c84c4b5827ac26faee3a8ad
John Thomas Moye <forglory0905@outlook.com>
2023-03-12 17:28:53 +0200

    add store and reducer

--- skills-test-react/src/index.js
+++ skills-test-react/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BasicForm, Home } from './pages';
+import store from './store';
 
 const router = createBrowserRouter([
   {
@@ -26,7 +28,9 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
       <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
