5121891f3f2f7fdb0f2f22e8404be719dcd297df
John Thomas Moye <forglory0905@outlook.com>
2023-03-12 17:30:07 +0200

    implement add & remote todo

--- skills-test-react/src/pages/BasicForm.js
+++ skills-test-react/src/pages/BasicForm.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Formik, Field, Form } from 'formik';
-import { Button } from '@mui/material';
+import { Button, Table, TableCell, TableRow } from '@mui/material';
+import {
+  addToDo,
+  selectTodos,
+  deleteTodo,
+} from '../reducer';
+import { useDispatch, useSelector } from 'react-redux';
 
-const BasicForm = () => (
-  <div>
-    <h1>TODO</h1>
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-      }}
-      onSubmit={(values) => {}}
-    >
-      <Form>
-        <label htmlFor="todo">Add ToDo </label>
-        <Field id="todo" name="Add ToDo" placeholder="TextHere" />
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
-  </div>
-);
+const BasicForm = () => {
+  const todos = useSelector(selectTodos);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>TODO</h1>
+      <Formik
+        initialValues={{
+          todo: '',
+        }}
+        onSubmit={(values, actions) => {
+          dispatch(addToDo(values.todo));
+          actions.resetForm();
+        }}
+      >
+        <Form>
+          <label htmlFor="todo">Add ToDo </label>
+          <Field id="todo" name="todo" placeholder="TextHere" />
+          <Button type="submit">Submit</Button>
+        </Form>
+      </Formik>
+      <h2>TODO List</h2>
+      <Table>
+        {
+          todos.map(todo => (
+            <TableRow key={todo.id}>
+              <TableCell>{todo.todo}</TableCell>
+              <TableCell>
+                <Button onClick={() => dispatch(deleteTodo(todo.id))}>Delete</Button>
+              </TableCell>
+            </TableRow>
+          ))
+        }
+      </Table>
+    </div>
+  );
+}
 
 export default BasicForm
